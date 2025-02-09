6ad7f4912123ed6f57ef3257fee54e00430a6d4c
Tomas Herrera <herrerabenavidestomasesteban@gmail.com>
2025-02-08 22:05:22 -0500

    commit

--- app/routes/movements/route.tsx
+++ app/routes/movements/route.tsx
@@ -1,5 +1,32 @@
+import "./movements.css";
+
 export default function Index() {
-    const hola = "Hola movimiento";
-  
-    return <div>{hola}</div>;
+
+return (<body>
+
+  <div className="container ">
+      <h2>Historial de Movimientos</h2>
+      <div className="transaction ">
+          <div>
+              <div><strong>Enviaste a Juan Pérez</strong></div>
+              <div className="status">Enviaste • 05 Feb 2025</div>
+          </div>
+          <div className="amount sent">- $50000.00</div>
+      </div>
+      <div className="transaction">
+          <div>
+              <div><strong>Transferencia recibida de Mariana Hernandez</strong></div>
+              <div className="status">Recibiste • 04 Feb 2025</div>
+          </div>
+          <div className="amount received">+ $1,200.00</div>
+      </div>
+      <div className="transaction">
+          <div>
+              <div><strong>Enviaste a Maty Herrera</strong></div>
+              <div className="status">Enviaste • 03 Feb 2025</div>
+          </div>
+          <div className="amount sent">- $250000.00</div>
+      </div>
+  </div>
+</body>)
   }
